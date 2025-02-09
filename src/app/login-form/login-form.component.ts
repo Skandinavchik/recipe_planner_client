c512121ac4abeb7b78277b3d75b2e3df6c54bb78
Artem Sokolov <sknd2569@gmail.com>
2025-02-09 18:02:33 +0100

    initiated login form with simple validation

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Component, inject } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
-  title = 'login form'
+  private formBuilder = inject(FormBuilder)
+  private http = inject(HttpClient)
+
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  })
+
+  onSubmit() {
+    if (this.loginForm.status === 'VALID') {
+      this.http.post('', this.loginForm.value).subscribe({
+        next: response => {
+          console.log('Login successful:', response)
+          this.loginForm.reset()
+        },
+        error: error => {
+          console.error('Login failed:', error)
+        },
+      })
+    }
+  }
 }
